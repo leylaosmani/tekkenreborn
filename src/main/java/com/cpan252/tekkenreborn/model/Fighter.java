793890aaee5902aab21e475f586bea7acdac9c0d
Leyla <Leyla.osmani@outlook.com>
2023-01-28 17:43:25 -0500

    Included design page&controller, fighter/fighterpool models

---
+++ src/main/java/com/cpan252/tekkenreborn/model/Fighter.java
@@ -0,0 +1,40 @@
+package com.cpan252.tekkenreborn.model;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Fighter {
+    @NotBlank
+    private String name;
+    @Max(100)
+    private int damagePerHit;
+    @Min(1000)
+    private int health;
+    @DecimalMin(value = "0.1", inclusive = true)
+    @DecimalMax(value = "10.0", inclusive = true)
+    private BigDecimal resistance;
+    private Anime animeFrom;
+
+    public enum Anime {
+        ATTACK_ON_TITAN("Attack on Titan"), DEATH_NOTE("Death Note"),BLEACH("Bleach"), TOKYO_REVENGERS("Tokyo Revengers"), ONE_PIECE("One Piece"), TEKKEN("Tekken");
+
+        private String title;
+
+        private Anime(String title) {
+            this.title = title;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+    }
+}
