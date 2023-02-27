f4f16094a6b16155c17b9154baf0e09fd8935d25
Leyla <Leyla.osmani@outlook.com>
2023-02-27 16:27:15 -0500

    LAB 5 (added search by name&dates and used JPA and implemented pagination for fighters

--- src/main/java/com/cpan252/tekkenreborn/repository/FighterRepository.java
+++ src/main/java/com/cpan252/tekkenreborn/repository/FighterRepository.java
@@ -1,8 +1,13 @@
 package com.cpan252.tekkenreborn.repository;
-
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 import com.cpan252.tekkenreborn.model.Fighter;
+import com.cpan252.tekkenreborn.model.Fighter.Anime;
 
 public interface FighterRepository extends CrudRepository<Fighter, Long> {
+    List<Fighter> findByAnimeFrom(Anime anime);
+
+    List<Fighter> findByNameStartsWithAndCreatedAtBetween(String name, LocalDate startDate, LocalDate endDate);
 }
